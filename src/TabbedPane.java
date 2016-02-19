8eaf8989a0788368d8a142f4406f7ab0bd9794b8
Marvin Shu <marvinshu@umail.ucsb.edu>
2016-02-18 16:08:32 -0800

    Added some comments to TabbedPane

--- src/TabbedPane.java
+++ src/TabbedPane.java
@@ -5,65 +5,73 @@
 import java.awt.*;
 import javax.swing.*;
 
-class TabbedPane
-		extends 	JFrame
+/**
+This TabbedPane shows how to play poker. There will be 3 tabs: one for
+general rules, one for betting, and one for hand rankings
+
+@author Marvin Shu
+@author Samuel E. Chu
+@version CS56, W16, P. Conrad
+ **/
+class TabbedPane extends  JFrame
 {
-	private		JTabbedPane tabbedPane;
-	private		JPanel		panel1;
-	private		JPanel		panel2;
-	private		JPanel		panel3;
+    private JTabbedPane tabbedPane;
+    private JPanel	panel1;
+    private JPanel    	panel2;
+    private JPanel	panel3;
 
 
-	public TabbedPane()
-	{
-		// NOTE: to reduce the amount of code in this example, it uses
-		// panels with a NULL layout.  This is NOT suitable for
-		// production code since it may not display correctly for
-		// a look-and-feel.
+    public TabbedPane()
+    {
+	// NOTE: to reduce the amount of code in this example, it uses
+	// panels with a NULL layout.  This is NOT suitable for
+	// production code since it may not display correctly for
+	// a look-and-feel.
 		
-		setTitle( "Poker Instructions" );
-		setSize( 796, 1000 );
-		setBackground( Color.gray );
+	setTitle( "Poker Instructions" );
+	setSize( 796, 1000 );
+	setBackground( Color.gray );
 
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout( new BorderLayout() );
-		getContentPane().add( topPanel );
+	JPanel topPanel = new JPanel();
+	topPanel.setLayout( new BorderLayout() );
+	getContentPane().add( topPanel );
 
-		// Create the tab pages
-		createPage1();
-		createPage2();
-		createPage3();
+	// Create the tab pages
+	createPage1();
+	createPage2();
+	createPage3();
 
-		// Create a tabbed pane
-		tabbedPane = new JTabbedPane();
-		tabbedPane.addTab( "General Rules", panel1 );
-		tabbedPane.addTab( "Betting", panel2 );
-		tabbedPane.addTab( "Hand Rankings", panel3 );
-		topPanel.add( tabbedPane, BorderLayout.CENTER );
-	}
-
-	public void createPage1()
-	{
-		ImageIcon image = new ImageIcon("images/instructions1.jpg");
-                JLabel label = new JLabel("", image, JLabel.CENTER);
-                panel1 = new JPanel(new BorderLayout());
-                panel1.add( label, BorderLayout.CENTER );
-	}
-
-	public void createPage2()
-	{
-		ImageIcon image = new ImageIcon("images/instructions2.jpg");
-                JLabel label = new JLabel("", image, JLabel.CENTER);
-                panel2 = new JPanel(new BorderLayout());
-                panel2.add( label, BorderLayout.CENTER );
-	}
-
-	public void createPage3()
-	{
-		ImageIcon image = new ImageIcon("images/poker_hand_rankings.jpg");
-                JLabel label = new JLabel("", image, JLabel.CENTER);
-                panel3 = new JPanel(new BorderLayout());
-                panel3.add( label, BorderLayout.CENTER );
-	}
+	// Create a tabbed pane
+	tabbedPane = new JTabbedPane();
+	tabbedPane.addTab( "General Rules", panel1 );
+	tabbedPane.addTab( "Betting", panel2 );
+	tabbedPane.addTab( "Hand Rankings", panel3 );
+	topPanel.add( tabbedPane, BorderLayout.CENTER );
+    }
+    
+    //create the first tabbed page, "General Rules"
+    public void createPage1()
+    {
+	ImageIcon image = new ImageIcon("images/instructions1.jpg");
+	JLabel label = new JLabel("", image, JLabel.CENTER);
+	panel1 = new JPanel(new BorderLayout());
+	panel1.add( label, BorderLayout.CENTER );
+    }
+    //create the second tabbed page, "Betting"
+    public void createPage2()
+    {
+	ImageIcon image = new ImageIcon("images/instructions2.jpg");
+	JLabel label = new JLabel("", image, JLabel.CENTER);
+	panel2 = new JPanel(new BorderLayout());
+	panel2.add( label, BorderLayout.CENTER );
+    }
+    //create the third tabbed page, "Hand Rankings"
+    public void createPage3()
+    {
+	ImageIcon image = new ImageIcon("images/poker_hand_rankings.jpg");
+	JLabel label = new JLabel("", image, JLabel.CENTER);
+	panel3 = new JPanel(new BorderLayout());
+	panel3.add( label, BorderLayout.CENTER );
+    }
 
 }
